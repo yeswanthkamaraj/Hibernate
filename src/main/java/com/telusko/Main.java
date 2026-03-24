ab74a72fdc3bb18b2c120f69b7a3912c5d289473
yeswanthkamaraj <yeswanthkamaraj25@gmail.com>
2026-03-24 12:18:14 +0530

    perform the merge operation with PostgreDB

--- src/main/java/com/telusko/Main.java
+++ src/main/java/com/telusko/Main.java
@@ -9,18 +9,24 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     static void main(String[] args) {
         Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setAname("Yeswanth");
-        a1.setTech("java");
+        a1.setAid(104);
+        a1.setAname("Kiran");
+        a1.setTech("AI/ML");
 
 
         Configuration config = new Configuration();
         config.addAnnotatedClass(com.telusko.Alien.class);
         config.configure();
-        SessionFactory factory = config.buildSessionFactory();
+
+       SessionFactory factory = config.buildSessionFactory();
         Session session = factory.openSession();
         Transaction transaction= session.beginTransaction();
-        session.persist(a1);
+
+        // Alien a5= session.find(Alien.class,103);
+        // System.out.println(a5);
+        session.merge(a1);
         transaction.commit();
+        session.close();
+        factory.close();
     }
 }
